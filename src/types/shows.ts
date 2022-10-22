47874a944d6cb812208bc7caee5a51b56d3c6c8d
Shivam Mishra <scm.mymail@gmail.com>
2022-10-22 13:22:08 +0530

    feat: add shows types

---
+++ src/types/shows.ts
@@ -0,0 +1,39 @@
+import type Genre from "./genre";
+
+export interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string;
+  season_number: number;
+}
+
+export interface TvShowDetails {
+  backdrop_path: string;
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  name: string;
+  next_episode_to_air?: any;
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  seasons: Season[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
