f89829ce43d5887dc4a9933600573b1fa95d689a
Shivam Mishra <scm.mymail@gmail.com>
2022-10-22 12:41:29 +0530

    feat: add types for movies

---
+++ src/types/movies.ts
@@ -0,0 +1,56 @@
+import type Genre from "./genre";
+
+export interface Movie {
+  id: number;
+  logo_path: string;
+  name: string;
+  poster_path: string;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: number[];
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection?: any;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path?: any;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieListResponse {
+  dates?: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: Movie[];
+  total_results: number;
+  total_pages: number;
+}
